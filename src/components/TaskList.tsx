000cdec5ba14f794789c2a7d707108e477dfad99
Saullo Bueno <saullobueno@gmail.com>
2022-03-16 17:41:16 -0300

    First solution finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,14 +15,15 @@ export function TaskList() {
 	function handleCreateNewTask() {
 		// Crie uma nova task com um id random, não permita criar caso o título seja vazio.
 		newTaskTitle &&
-			setTasks([
-				...tasks,
+			setTasks((oldTasks) => [
+				...oldTasks,
 				{
 					id: Date.now(),
 					title: newTaskTitle,
 					isComplete: false,
 				},
 			]);
+		setNewTaskTitle('');
 	}
 
 	function handleToggleTaskCompletion(id: number) {
